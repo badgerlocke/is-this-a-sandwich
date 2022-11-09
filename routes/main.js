ce8fca52b3ce44188d5302fb18e27ecb8ca97955
Sage <hwilliams6047@gmail.com>
2022-11-08 22:08:43 -0500

    Change 'profile.ejs' to 'home.ejs'

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/home", ensureAuth, postsController.getHome);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login-trial", authController.getLogin);
 router.post("/login-trial", authController.postLogin);
