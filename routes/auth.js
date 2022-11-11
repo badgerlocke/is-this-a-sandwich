02c2631038ed8e5096605386185fe22abb5ae851
Sage <hwilliams6047@gmail.com>
2022-11-10 23:42:18 -0500

    Add 'email' to scope

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,8 @@ const router = express.Router()
 
 // @desc    Auth with Google
 // @route   GET /auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+// Scope necessary to get user ID and email
+router.get('/google', passport.authenticate('google', { scope: ['profile','email'] }))
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
